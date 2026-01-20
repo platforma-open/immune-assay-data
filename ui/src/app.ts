f10bacb01f8c9076d1d9a44660a4be70f4c9d1cd
Mariia Zueva <maryzueva@gmail.com>
2026-01-20 12:43:04 +0100

    Labels migration

--- ui/src/app.ts
+++ ui/src/app.ts
@@ -1,5 +1,6 @@
 import { model } from '@platforma-open/milaboratories.immune-assay-data.model';
 import { defineApp } from '@platforma-sdk/ui-vue';
+import { watch } from 'vue';
 import MainPage from './pages/MainPage.vue';
 
 export const sdkPlugin = defineApp(model, () => {
@@ -11,3 +12,12 @@ export const sdkPlugin = defineApp(model, () => {
 });
 
 export const useApp = sdkPlugin.useApp;
+
+// Make sure labels are initialized
+const unwatch = watch(sdkPlugin, ({ loaded }) => {
+  if (!loaded) return;
+  const app = useApp();
+  app.model.args.customBlockLabel ??= '';
+  app.model.args.defaultBlockLabel ??= 'Select Dataset';
+  unwatch();
+});
