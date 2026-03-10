3e7c0c218674afeb424e565a1c5bb38698ad355b
Julen Mendieta <jmendieta@milaboratories.com>
2026-03-10 14:29:41 +0100

    Make block work with empty inputs

--- model/src/index.ts
+++ model/src/index.ts
@@ -179,7 +179,7 @@ export const model = BlockModel.create()
   )
 
   .outputWithStatus('table', (ctx) => {
-    if (ctx.outputs?.resolve('emptyResults')?.getDataAsJson<boolean>()) {
+    if (ctx.outputs?.resolve('emptyClonesInput')?.getDataAsJson<string>() === 'empty') {
       return undefined;
     }
     const cols = ctx.outputs?.resolve('table')?.getPColumns();
@@ -194,7 +194,7 @@ export const model = BlockModel.create()
   })
 
   .output('pf', (ctx) => {
-    if (ctx.outputs?.resolve('emptyResults')?.getDataAsJson<boolean>()) {
+    if (ctx.outputs?.resolve('emptyClonesInput')?.getDataAsJson<string>() === 'empty') {
       return undefined;
     }
     const cols = ctx.outputs?.resolve('table')?.getPColumns();
@@ -205,7 +205,7 @@ export const model = BlockModel.create()
   })
 
   .output('assaySequenceSpec', (ctx): PColumnSpec | undefined => {
-    if (ctx.outputs?.resolve('emptyResults')?.getDataAsJson<boolean>()) {
+    if (ctx.outputs?.resolve('emptyClonesInput')?.getDataAsJson<string>() === 'empty') {
       return undefined;
     }
     const cols = ctx.outputs?.resolve('table')?.getPColumns();
@@ -217,7 +217,7 @@ export const model = BlockModel.create()
   })
 
   .output('msaPf', (ctx) => {
-    if (ctx.outputs?.resolve('emptyResults')?.getDataAsJson<boolean>()) {
+    if (ctx.outputs?.resolve('emptyClonesInput')?.getDataAsJson<string>() === 'empty') {
       return undefined;
     }
     const cols = ctx.outputs?.resolve('table')?.getPColumns();
@@ -234,6 +234,10 @@ export const model = BlockModel.create()
 
     return createPFrameForGraphs(ctx, [...msaCols, ...cols, ...columns.props]);
   })
+
+  .output('emptyClonesInput', (ctx) =>
+    ctx.outputs?.resolve('emptyClonesInput')?.getDataAsJson<string>() === 'empty',
+  )
 
   .output('isRunning', (ctx) => ctx.outputs?.getIsReadyOrError() === false)
 
